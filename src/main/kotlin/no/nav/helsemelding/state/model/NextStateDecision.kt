76d9e022c05d8ed053930e6bb2d8dfc820fa5ddf
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2026-02-17 13:14:59 +0100

    Aligned log patterns (#63)

--- src/main/kotlin/no/nav/helsemelding/state/model/NextStateDecision.kt
+++ src/main/kotlin/no/nav/helsemelding/state/model/NextStateDecision.kt
@@ -5,15 +5,17 @@ sealed interface NextStateDecision {
         override fun toString() = "UNCHANGED"
     }
 
-    data class Transition(val to: MessageDeliveryState) : NextStateDecision
+    data class Transition(val to: MessageDeliveryState) : NextStateDecision {
+        override fun toString() = "TRANSITION($to)"
+    }
 
     sealed interface Rejected : NextStateDecision {
         data object Transport : Rejected {
-            override fun toString() = "TRANSPORT_REJECTED"
+            override fun toString() = "REJECTED(TRANSPORT)"
         }
 
         data object AppRec : Rejected {
-            override fun toString() = "APP_REC_REJECTED"
+            override fun toString() = "REJECTED(APPREC)"
         }
     }
 }
